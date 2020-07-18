f77ba194d8b9cfb3320001d30d73d0ceb5fc4daf
Shakil-Ahmed-dk <shakil.ons@gmail.com>
2020-07-17 23:57:03 -0700

    second

--- assignment.js
+++ assignment.js
@@ -1,63 +1,69 @@
 // feetToMile
 
-// function feetToMile(feet){
-//     if(feet>=0){
-//         var mile = feet/5280;
-//          return mile;
-//     }
-//     else{
-//         var warning="Warning! Distance value cannot be negative";
-//         return warning;
-//     }
+function feetToMile(feet){
+    if(feet>=0){
+        var mile = feet/5280;
+         return mile;
+    }
+    else{
+        var warning="Warning! Distance value cannot be negative";
+        return warning;
+    }
     
-// }
+}
 
-// var mileResult = feetToMile(0);
-// console.log(mileResult);
+var mileResult = feetToMile(1);
+console.log(mileResult);
 
 // woodCalculator
 
 
-// function woodCalculator(table, chair, bed){
-//     var tableWoodCount= table*1;
-//     var chairWoodCount= chair*3;
-//     var bedWoodCount= bed*5;
+function woodCalculator(table, chair, bed){
+    if(table<0 ||chair<0 || bed<0)
+    {return "Invalid Input number!"}
 
-//     var totalWoodCount= tableWoodCount+chairWoodCount+bedWoodCount;
-//     return totalWoodCount;
-// }
+    else{
+        var tableWoodCount= table*1;
+        var chairWoodCount= chair*3;
+        var bedWoodCount= bed*5;
 
-// var woodResult=woodCalculator(10,5,5);
-// console.log(woodResult);
+        var totalWoodCount= tableWoodCount+chairWoodCount+bedWoodCount;
+        return totalWoodCount;
+    }
+    
+}
+
+var woodResult=woodCalculator(10,5,5);
+console.log(woodResult);
 
 
 // brickCalculator
 
-// function brickCalculator(floor){
-//     if(floor>=0 && floor%1==0){
-//         if(floor<=10){
-//             var brickcount = floor*15*1000;
-//             return brickcount;
-//         }
-//         if(floor<=20){
-//             var level2 = floor-10;
-//             var brickcount = (10*15*1000)+(level2*12*1000);
-//             return brickcount;
-//         }
+function brickCalculator(floor){
+    if(floor>=0 && floor%1==0){
+        if(floor<=10){
+            var brickcount = floor*15*1000;
+            return brickcount;
+        }
+        if(floor<=20){
+            var level2 = floor-10;
+            var brickcount = (10*15*1000)+(level2*12*1000);
+            return brickcount;
+        }
     
-//         if(floor>20){
-//             var level3 = floor-20;
-//             var brickcount = (10*15*1000)+(10*12*1000)+(level3*10*1000);
-//             return brickcount;
-//         }
-//     }
-//     else{
-//         return "Invalide Floor number! ";
-//     }
-// }
+        if(floor>20){
+            var level3 = floor-20;
+            var brickcount = (10*15*1000)+(10*12*1000)+(level3*10*1000);
+            return brickcount;
+        }
+    }
+    else{
+        return "Invalide Floor number! ";
+    }
+}
 
-// var totalBrick = brickCalculator(1);
-// console.log(totalBrick);
+var totalBrick = brickCalculator(1);
+console.log(totalBrick);
 
 
 
@@ -66,25 +72,22 @@
 function tinyFriend(n){
     var smallest=n[0];
     var nameLength= smallest.length;
-    
+    var j=0;
     
      for(var i=0; i<n.length;i++){
+         
          var currentName= n[i];
-        //  console.log(currentName);
          var currentNameLength=currentName.length;
       
 
          if(currentNameLength<nameLength){
             nameLength= currentNameLength;
-            var final=n[i];
+            var j=currentName;
          }
      }
-   
-     console.log(n);
- console.log(nameLength);
-
+     return j;
+    //  return nameLength;
 }
-tinyFriend(["shakil", "iftekhar", "kabiravai"]);
 
-// var small=tinyFriend(["shakil", "iftekhar", "kabiravai"]);
-//  console.log(small);
+var small=tinyFriend(["sh", "ilg", "kabiravai"]);
+ console.log(small);
